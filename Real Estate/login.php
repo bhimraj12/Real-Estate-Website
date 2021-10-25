dc2bea50d0fc627b42ed82fedafba683863b9748
shridhar-coder <shridharjoshi1993@gmail.com>
2021-10-26 00:05:59 +0530

    added login form

--- Real Estate/login.php
+++ Real Estate/login.php
@@ -1,3 +1,35 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="real_estate";
+
+mysql_connect($host,$user,$password);
+$mysql_select_db($db);
+
+if(isset($_POST['username'])){
+    $uname= $_POST['username'];
+    $password=$_POST['password'];
+
+    $sql="select * from account_details where USERNAME='".$uname."' AND PASSWORD='".$password."' limit 1";
+    $result=mysql_query($sql);
+    if(mysql_num_rows($result)==1){
+        echo "You have Successfully Logged in.";
+        exit();
+    }
+    else{
+        echo "Login Failed. Please try again.";
+        exit();
+    }
+}
+
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -41,7 +73,7 @@
     <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <form action="#" class="sign-in-form">
+                <form method="POST" action="#" class="sign-in-form">
                     <h2 class="title">Sign in</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
